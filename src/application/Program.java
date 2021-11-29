fa4293f5d266a926390c7ac828eec40baae3dc6f
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2021-11-29 11:23:45 -0300

    Comparator objeto de expressão lambda com chave

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,6 +9,7 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
+		/* Expressao lambida: função anonima	*/
 		
 		List<Product> list = new ArrayList<>();
 		
@@ -17,14 +17,9 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		//cria uma classe anonima para definir a comparação
-		Comparator<Product> comp = new Comparator<Product>() {
-
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
-			
+		//cria uma função anonima usando a expressao lambida
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
 		
 		list.sort(comp);//organiza em ordem alfabetica
