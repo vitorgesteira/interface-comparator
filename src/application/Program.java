7c4226a1ef1055ea540abbb549cab5f2f7cd4bdf
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2021-11-29 11:29:05 -0300

    Comparator objeto de expressão lambda sem chaves

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,9 +18,7 @@ public class Program {
 		list.add(new Product("Tablet", 450.00));
 		
 		//cria uma função anonima usando a expressao lambida
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		
 		list.sort(comp);//organiza em ordem alfabetica
 		
